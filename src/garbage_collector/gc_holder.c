b67ae64cf878949216e3f746c21116332a4b2094
Lioserious <lihrig@student.42heilbronn.de>
2025-05-01 12:42:07 +0200

    ADD(garbage_collector) —> added garbage collector_ADD and garbage collect_EMPTY

--- src/garbage_collector/gc_holder.c
+++ src/garbage_collector/gc_holder.c
@@ -6,3 +6,28 @@ t_list	**garbage_holder(void)
 
 	return (&ptr);
 }
+
+void garbage_collector_add(void *ptr)
+{
+	t_list	*neu;
+
+	neu = ft_lstnew(ptr);
+	if(!neu)
+		error_handler("MEMORY ALLOCATION FAILED: GARBAGE COLLECTOR");
+	ft_lstadd_front(garbage_holder(), neu);
+
+}
+
+void garbage_collector_empty(void)
+{
+	t_list	**holder;
+	t_list	*temp;
+
+	holder = garbage_holder();
+	while(*holder != NULL);
+	{
+		temp = *holder;
+		holder = (*holder)->next;
+		ft_lstdelone(temp, free);
+	}
+}

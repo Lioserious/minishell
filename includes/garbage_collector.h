9cedb9a65e1e3957db16369e8287ecb041d5c7c1
Lioserious <lihrig@student.42heilbronn.de>
2025-05-01 20:00:45 +0200

    BUILD(Garbage collector.h) —> added gc_malloc

--- includes/garbage_collector.h
+++ includes/garbage_collector.h
@@ -7,6 +7,7 @@ t_list	**garbage_holder(void);
 void	garbage_collector_add(void *ptr);
 void	garbage_collector_empty(void);
 void	garbage_collector_print(void);
+void	*gc_malloc(size_t size);
 
 #endif
 

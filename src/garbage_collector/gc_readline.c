d08f2b7708ccf716c0477e7138e703388f43844f
Lioserious <lihrig@student.42heilbronn.de>
2025-05-02 13:46:34 +0200

    FEAT(garbage_collector): Add gc_readline function for memory-managed input handling

--- src/garbage_collector/gc_readline.c
+++ src/garbage_collector/gc_readline.c
@@ -6,7 +6,20 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 13:38:41 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/02 13:38:41 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/02 13:45:09 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+// eigene readline implementierung, die den input Pointer direkt in den GC
+// anhaengt.
+char	*gc_readline(const char *prompt)
+{
+	char *input;
+
+	input = readline(prompt);
+	if (input)
+		garbage_collector_add(input);
+	return (input);
+}

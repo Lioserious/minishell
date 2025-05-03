337386b68f0e0d8b679a77dd02f82cdad36a331b
Lioserious <lihrig@student.42heilbronn.de>
2025-05-03 13:45:09 +0200

    ADD(tokenizer.h) —> Home to the struct/linked list of the tokenizer also added to minishell .h

--- includes/tokenizer.h
+++ includes/tokenizer.h
@@ -1,6 +1,22 @@
 #ifndef TOKENIZER_H
 # define TOKENIZER_H
 
+typedef enum e_token_type
+{
+    TOKEN_WORD,         // Normale Wörter
+    TOKEN_PIPE,         // |
+    TOKEN_REDIR_IN,     // 
+    TOKEN_REDIR_OUT,    // >
+    TOKEN_REDIR_APPEND, // >>
+    TOKEN_REDIR_HEREDOC,// 
+    TOKEN_EOF           // Ende des Inputs
+} t_token_type;
 
+typedef struct s_token
+{
+    t_token_type    type;
+    char            *value;
+    struct s_token  *next;
+} t_token;
 
 #endif

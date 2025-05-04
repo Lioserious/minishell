84bcb06a982afafdba4f75e2bc91fb8d4c346273
Lioserious <lihrig@student.42heilbronn.de>
2025-05-04 12:42:41 +0200

    BUILD(tokenizer) —> tokenizer linked list function initaliser

--- includes/tokenizer.h
+++ includes/tokenizer.h
@@ -1,8 +1,6 @@
 #ifndef TOKENIZER_H
 # define TOKENIZER_H
 
-
-
 typedef enum e_token_type
 {
     TOKEN_WORD,         // Normale Wörter
@@ -14,11 +12,18 @@ typedef enum e_token_type
     TOKEN_EOF           // Ende des Inputs
 } t_token_type;
 
+typedef struct s_token_list
+{
+    t_token     *head;
+    t_token     *tail;
+    ssize_t     size;
+}               t_token_list;
+
 typedef struct s_token
 {
     t_token_type    type;
     char            *value;
     struct s_token  *next;
-} t_token;
+}                   t_token;
 
 #endif
--- src/tokenizer/tokenizer_utils.c
+++ src/tokenizer/tokenizer_utils.c
@@ -6,7 +6,24 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 12:26:41 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/04 12:26:41 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/04 12:41:03 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+// creates Linked List for tokens
+t_token_list *create_token_list(void)
+{
+	t_token_list *new_list;
+	
+	new_list = (t_token_list *)gc_malloc(sizeof(t_token_list));
+	if(!new_list)
+		error_handler("MEMORY ALLOCATION FAILED: CREATE_TOKEN_LIST", 1);
+	new_list->head = NULL;
+	new_list->tail = NULL;
+	new_list->size = 0;
+	return(new_list);
+}
+//creates token nodes
+t_token *create_token(t_token_type type)

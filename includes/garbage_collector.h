074664164a92d35de7f0c101ecd68ff5bf31e79c
Lioserious <lihrig@student.42heilbronn.de>
2025-05-04 13:24:25 +0200

    FEAT(gc_substr) —> substring function that includes garbage collector

--- includes/garbage_collector.h
+++ includes/garbage_collector.h
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:01:43 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/04 12:48:38 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/04 13:22:04 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void	garbage_collector_print(void);
 void	*gc_malloc(size_t size);
 char	*gc_readline(const char *prompt);
 char	*gc_strdup(const char *s);
+char	*gc_substr(char const *s, unsigned int start, size_t len);
 
 #endif

2ad4a14e108af3d044a1b2b525860fdb3db98e39
Lioserious <lihrig@student.42heilbronn.de>
2025-05-04 16:23:39 +0200

    FEAT(double_single_quotes) -> double  quotes

--- src/tokenizer/handle_quotes.c.c
+++ src/tokenizer/handle_quotes.c.c
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 15:48:11 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/04 16:05:10 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/04 16:22:39 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,70 @@ int	handle_single_quote(char *input, int *i, t_token_list *token_list)
 	return (1);
 }
 
+// Speichert Text vor einer Umgebungsvariable
+static void	save_text_before_var(char *input, int start, int end,
+		t_token_list *token_list)
+{
+	int		len;
+	char	*value;
+	t_token	*new_token;
+
+	if (end > start)
+	{
+		len = end - start;
+		value = gc_substr(input, start, len);
+		new_token = create_token(TOKEN_WORD, value);
+		add_token_to_list(token_list, new_token);
+	}
+}
+
+// Speichert verbleibenden Text nach der letzten Variable
+static void	save_remaining_text(char *input, int start, int end,
+		t_token_list *token_list)
+{
+	int		len;
+	char	*value;
+	t_token	*new_token;
+
+	if (end > start)
+	{
+		len = end - start;
+		value = gc_substr(input, start, len);
+		new_token = create_token(TOKEN_WORD, value);
+		add_token_to_list(token_list, new_token);
+	}
+}
+// Verarbeitet den Inhalt innerhalb der doppelten Anführungszeichen
+static void	process_double_quote_content(char *input, int *i,
+		t_token_list *token_list)
+{
+	int	start;
+
+	start = *i;
+	while (input[*i] && input[*i] != '"')
+	{
+		if (input[*i] == '$')
+		{
+			save_text_before_var(input, start, *i, token_list);
+			handle_env_var(input, i, token_list);
+			start = *i;
+		}
+		else
+			(*i)++;
+	}
+	if (!input[*i])
+		error_handler("UNCLOSED DOUBLE QUOTE", 1);
+	save_remaining_text(input, start, *i, token_list);
+	return ;
+}
+
+// Hauptfunktion für doppelte Anführungszeichen
 int	handle_double_quote(char *input, int *i, t_token_list *token_list)
 {
-	t_token *new_token;
-	int start;
-	int len;
-	char *value;
-
 	if (input[*i] != '"')
 		return (0);
 	(*i)++;
-	start = *i;
-	while (input[*i] && input[*i] != '"')
+	process_double_quote_content(input, i, token_list);
+	(*i)++;
+	return (1);
 }

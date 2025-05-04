042ab7aed01b3efc50a57f16bfd04d8ab0c1bb9a
Lioserious <lihrig@student.42heilbronn.de>
2025-05-04 15:44:20 +0200

    FEAT(handle_env_var)-> function that handles env variables within the tokenizer

--- src/tokenizer/handle_env_variable.c
+++ src/tokenizer/handle_env_variable.c
@@ -6,8 +6,43 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 14:42:04 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/04 14:42:19 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/04 15:41:52 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// Environment-Variablen in Unix/Linux folgende Regeln haben:
+// Sie dürfen Buchstaben (a-z, A-Z) enthalten
+// Sie dürfen Ziffern (0-9) enthalten
+// Sie dürfen Underscores (_) enthalten
+// Sie müssen mit einem Buchstaben oder Underscore beginnen (nicht mit einer Ziffer)
+
+int	handle_env_var(char *input, int *i, t_token_list *token_list)
+{
+	t_token	*new_token;
+	int		start;
+	int		len;
+	char	*value;
+
+	if (input[*i] != '$')
+		return (0);
+	start = *i;
+	if (input[*i] == '?')
+	{
+		(*i)++;
+		new_token = create_token(TOKEN_ENV_VAR, "$?");
+		return (add_token_to_list(token_list, new_token), 1);
+	}
+	while (input[*i] && (ft_isalnum(input[*i]) || input[*i] == '_'))
+		(*i)++;
+	len = *i - start;
+	if (len == 1)
+	{
+		new_token = create_token(TOKEN_WORD, "$");
+		return (add_token_to_list(token_list, new_token), 1);
+	}
+	value = gc_substr(input, start, len);
+	new_token = create_token(TOKEN_ENV_VAR, value);
+	return (1);
+}

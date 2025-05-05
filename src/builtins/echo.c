c6cde44c83207661125bd10a30e52f04b8d4761c
Miguel Malek <mimalek@2-E-3.42heilbronn.de>
2025-05-05 12:56:52 +0200

    FEAT(echo.c) -> Implemented echo-builtin

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,21 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execute_builtins.c                                 :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/04 18:43:20 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/05 11:44:07 by mimalek          ###   ########.fr       */
+/*   Created: 2025/05/05 11:16:33 by mimalek           #+#    #+#             */
+/*   Updated: 2025/05/05 11:45:27 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	execute_builtin(t_cmd_node *node)
+int	ft_echo(t_cmd_node *node)
 {
-	if (ft_strncmp(node->cmd[0], "pwd", 4) == 0)
-		ft_pwd();
-	else if (ft_strncmp(node->cmd[0], "echo", 5) == 0)
-		ft_echo(node);
+	int	i;
+	int	newline;
+
+	i = 1;
+	newline = 1;
+	if (node->cmd[i] && ft_strncmp(node->cmd[i], "-n", 3) == 0)
+	{
+		newline = 0;
+		i++;
+	}
+	while(node->cmd[i])
+	{
+		ft_putstr_fd(node->cmd[i], 1);
+		if (node->cmd[i + 1])
+			write(1, " ", 1);
+		i++;
+	}
+	if (newline)
+		write(1, "\n", 1);
+	return (0);
 }

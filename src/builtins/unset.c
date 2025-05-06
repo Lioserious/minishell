79cf6452929ea53cde6c95dc5799d1a9243bba24
Miguel Malek <mimalek@2-D-2.42heilbronn.de>
2025-05-06 15:52:48 +0200

    NORM(unset.c) -> Norm Confirm

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,11 +6,36 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 12:18:21 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/06 12:52:23 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/06 15:52:03 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	remove_env_var(t_env_list *env_list, char *name)
+{
+	t_env_node	*current;
+	t_env_node	*previous;
+
+	current = env_list->head;
+	previous = NULL;
+	while (current)
+	{
+		if (ft_strncmp(current->name, name,
+				ft_strlen(name)) == 0
+			&& ft_strlen(current->name) == ft_strlen(name))
+		{
+			if (previous)
+				previous->next = current->next;
+			else
+				env_list->head = current->next;
+			env_list->count--;
+			break ;
+		}
+		previous = current;
+		current = current->next;
+	}
+}
 
 int	ft_unset(t_env_list *env_list, t_cmd_node *node)
 {
@@ -21,23 +46,7 @@ int	ft_unset(t_env_list *env_list, t_cmd_node *node)
 	i = 1;
 	while (node->cmd[i])
 	{
-		current = env_list->head;
-		previous = NULL;
-		while (current)
-		{
-			if (ft_strncmp(current->name, node->cmd[i], ft_strlen(node->cmd[i])) == 0
-			&& ft_strlen(current->name) == ft_strlen(node->cmd[i]))
-			{
-				if (previous)
-					previous->next = current->next;
-				else
-					env_list->head = current->next;
-				env_list->count--;
-				break ;
-			}
-			previous = current;
-			current = current->next;
-		}
+		remove_env_var(env_list, node->cmd[i]);
 		i++;
 	}
 	return (0);

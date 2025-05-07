34b9461e3e38c65632aa1a301b1746492ccea1d3
Miguel Malek <mimalek@2-H-2.42heilbronn.de>
2025-05-07 17:00:21 +0200

    ADD(builtins.h) -> specific header file for builtin functions

---
+++ includes/builtins.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 16:57:06 by mimalek           #+#    #+#             */
+/*   Updated: 2025/05/07 16:58:41 by mimalek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int		ft_pwd(void);
+int		ft_echo(t_cmd_node *node);
+int		ft_cd(t_cmd_node *node, t_env_list *env_list);
+int		ft_env(t_env_list *env_list);
+int		ft_export(t_env_list *env_list, t_cmd_node *node);
+int		ft_exit(t_cmd_node *node);
+int		ft_unset(t_env_list *env_list, t_cmd_node *node);

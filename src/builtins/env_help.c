658a6a793315c0a4be460c39a1815bf0267bfa66
Miguel Malek <mimalek@2-H-2.42heilbronn.de>
2025-05-07 16:54:55 +0200

    NORM(env_help.c) -> Created a new file for environment variable helper functions

---
+++ src/builtins/env_help.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_help.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 16:50:46 by mimalek           #+#    #+#             */
+/*   Updated: 2025/05/07 16:53:35 by mimalek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_add_env_var(t_env_list *env_list, char *name,
+	char *value, int is_export)
+{
+	t_env_node	*new_node;
+	t_env_node	*current;
+
+	if (!env_list || !name)
+		return ;
+	new_node = (t_env_node *)gc_malloc(sizeof(t_env_node));
+	if (!new_node)
+		return ;
+	new_node->name = gc_strdup(name);
+	if (value)
+		new_node->value = gc_strdup(value);
+	else
+		new_node->value = gc_strdup("");
+	new_node->is_export = is_export;
+	new_node->next = NULL;
+	if (!env_list->head)
+		env_list->head = new_node;
+	else
+	{
+		current = env_list->head;
+		while (current->next)
+			current = current->next;
+		current->next = new_node;
+	}
+	env_list->count++;
+}
+
+char	*get_env_value(t_env_list *env_list, char *name)
+{
+	t_env_node	*current;
+
+	current = env_list->head;
+	while (ft_strncmp(current->name, name, ft_strlen(name + 1)) != 0)
+		current = current->next;
+	return (current->value);
+}
+
+void	set_env_var(t_env_list *env_list, char *name, char *value)
+{
+	t_env_node	*current;
+
+	current = env_list->head;
+	while (current)
+	{
+		if (ft_strncmp(current->name, name, ft_strlen(name + 1)) == 0)
+		{
+			current->value = gc_strdup(value);
+			return ;
+		}
+		current = current->next;
+	}
+	return ;
+}

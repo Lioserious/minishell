d0f7172ba84322963562dadbca19923414bfb1e6
Miguel Malek <mimalek@2-H-2.42heilbronn.de>
2025-05-07 16:17:13 +0200

    CHORE(cd.c) -> Refactored the cd.c function to improve readability

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,42 +6,71 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 13:16:09 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/07 15:47:27 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/07 16:13:26 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_move_to_oldpwd(t_env_list *env_list);
+int		cd_to_home(t_env_list *env_list);
+int		cd_to_oldpath(t_env_list *env_list);
+int		cd_to_path(char *path, t_env_list	*env_list);
 
 int	ft_cd(t_cmd_node *node, t_env_list *env_list)
 {
-	char	*home;
 	char	current_dir[PATH_MAX];
-	char	cwd[PATH_MAX];
 
 	if (getcwd(current_dir, sizeof(current_dir)) != NULL)
 		set_env_var(env_list, "OLDPWD", gc_strdup(current_dir));
 	if (!node->cmd[1] || node->cmd[1][0] == '~')
 	{
-		home = getenv("HOME");
-		if (home == NULL || chdir(home) != 0)
-		{
-			perror("cd");
-			return (1);
-		}
-		set_env_var(env_list, "PWD", gc_strdup(home));
-		return (0);
+		return (cd_to_home(env_list));
 	}
 	if (node->cmd[1][0] == '-')
 	{
-		ft_move_to_oldpwd(env_list);
-		return (0);
+		return (cd_to_oldpath(env_list));
 	}
-	if (chdir(node->cmd[1]) != 0)
+	return (cd_to_path(node->cmd[1], env_list));
+}
+
+int	cd_to_home(t_env_list *env_list)
+{
+	char	*home;
+
+	home = getenv("HOME");
+	if (home == NULL || chdir(home) != 0)
+	{
+		perror("cd");
+		return (1);
+	}
+	set_env_var(env_list, "PWD", gc_strdup(home));
+	return (0);
+}
+
+int	cd_to_oldpath(t_env_list *env_list)
+{
+	char	*oldpwd;
+
+	oldpwd = get_env_value(env_list, "OLDPWD");
+	if (oldpwd == NULL || chdir(oldpwd) != 0)
+	{
+		ft_putstr_fd("cd: OLDPWD not set or invalid\n", 2);
+		return (1);
+	}
+	set_env_var(env_list, "OLDPWD", gc_strdup(oldpwd));
+	ft_putstr_fd(oldpwd, 1);
+	ft_putendl_fd("", 1);
+	return (0);
+}
+
+int	cd_to_path(char *path, t_env_list	*env_list)
+{
+	char	cwd[PATH_MAX];
+
+	if (chdir(path) != 0)
 	{
 		ft_putstr_fd("cd: ", 2);
-		ft_putstr_fd(node->cmd[1], 2);
+		ft_putstr_fd(path, 2);
 		ft_putstr_fd(": ", 2);
 		perror("");
 		return (1);
@@ -49,24 +78,4 @@ int	ft_cd(t_cmd_node *node, t_env_list *env_list)
 	if (getcwd(cwd, sizeof(cwd)))
 		set_env_var(env_list, "PWD", gc_strdup(cwd));
 	return (0);
-}
-
-void	ft_move_to_oldpwd(t_env_list *env_list)
-{
-	char		*oldpwd;
-	t_env_node	*current;
-
-	current = env_list->head;
-	while (ft_strncmp(current->name, "OLDPWD", 7) != 0)
-		current = current->next;
-	oldpwd = current->value;
-	if (chdir(oldpwd) != 0)
-	{
-		perror("cd");
-		return ;
-	}
-	ft_putstr_fd(oldpwd , 1);
-	ft_putendl_fd("", 1);
-	set_env_var(env_list, "PWD", gc_strdup(oldpwd));
-	return ;
 }

936005a328116c607784c6d3355914f4c9b71b55
Miguel Malek <mimalek@2-H-2.42heilbronn.de>
2025-05-07 16:55:46 +0200

    NORM(env.c) -> Made it more readable

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,11 +6,28 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 15:53:12 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/07 16:01:40 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/07 16:52:01 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	ft_env(t_env_list *env_list)
+{
+	t_env_node	*current;
+
+	if (!env_list)
+		return (1);
+	current = env_list->head;
+	while (current)
+	{
+		ft_putstr_fd(current->name, 1);
+		ft_putchar_fd('=', 1);
+		ft_putendl_fd(current->value, 1);
+		current = current->next;
+	}
+	return (0);
+}
 
 t_env_list	*init_env_list(void)
 {
@@ -20,36 +37,6 @@ t_env_list	*init_env_list(void)
 	env_list->head = NULL;
 	env_list->count = 0;
 	return (env_list);
-}
-
-void	ft_add_env_var(t_env_list *env_list, char *name,
-	char *value, int is_export)
-{
-	t_env_node	*new_node;
-	t_env_node	*current;
-
-	if (!env_list || !name)
-		return ;
-	new_node = (t_env_node *)gc_malloc(sizeof(t_env_node));
-	if (!new_node)
-		return ;
-	new_node->name = gc_strdup(name);
-	if (value)
-		new_node->value = gc_strdup(value);
-	else
-		new_node->value = gc_strdup("");
-	new_node->is_export = is_export;
-	new_node->next = NULL;
-	if (!env_list->head)
-		env_list->head = new_node;
-	else
-	{
-		current = env_list->head;
-		while (current->next)
-			current = current->next;
-		current->next = new_node;
-	}
-	env_list->count++;
 }
 
 void	init_env(t_env_list *env_list)
@@ -72,48 +59,4 @@ void	init_env(t_env_list *env_list)
 		}
 		env++;
 	}
-}
-
-void	set_env_var(t_env_list *env_list, char *name, char *value)
-{
-	t_env_node	*current;
-
-	current = env_list->head;
-	while(current)
-	{
-		if (ft_strncmp(current->name, name, ft_strlen(name + 1)) == 0)
-		{
-			current->value = gc_strdup(value);
-			return ;
-		}
-		current = current->next;
-	}
-	return ;
-}
-
-char	*get_env_value(t_env_list *env_list, char *name)
-{
-	t_env_node	*current;
-
-	current = env_list->head;
-	while (ft_strncmp(current->name, name, ft_strlen(name + 1)) != 0)
-		current = current->next;
-	return (current->value);
-}
-
-int	ft_env(t_env_list *env_list)
-{
-	t_env_node	*current;
-
-	if (!env_list)
-		return (1);
-	current = env_list->head;
-	while (current)
-	{
-		ft_putstr_fd(current->name, 1);
-		ft_putchar_fd('=', 1);
-		ft_putendl_fd(current->value, 1);
-		current = current->next;
-	}
-	return (0);
 }

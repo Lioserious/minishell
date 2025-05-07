7e13f135ea3b24f627adcc9039721b3542727edc
Miguel Malek <mimalek@2-H-2.42heilbronn.de>
2025-05-07 17:11:46 +0200

    CHORE(minishell.h) -> Added new includes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:01:55 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/04 19:15:27 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/07 17:03:59 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 # include "libft.h"
 # include "structs.h"
 # include "execution.h"
+# include "builtins.h"
+# include "env.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>

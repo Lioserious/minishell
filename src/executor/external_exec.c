a2f4bf5ea6b511d6ed5c8dfc0578d7394a621bc6
Miguel Malek <mimalek@3-G-5.42heilbronn.de>
2025-05-08 18:06:06 +0200

    ADD(external_exec.c -> Added the functionality to execute external commands

--- src/executor/external_exec.c
+++ src/executor/external_exec.c
@@ -6,18 +6,58 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 16:11:31 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/08 16:25:00 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/08 18:05:26 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void execute_external(node, env_list)
-{
+static char	*get_cmd_path(t_env_list *env_list, char *cmd);
+static void	handle_child_process(t_cmd_node *node,
+				t_env_list *env_list, char **enva);
 
+void	execute_external(t_cmd_node *node, t_env_list *env_list)
+{
+	char	**enva;
+	pid_t	pid;
+	int		status;
+
+	enva = convert_env_struct_array(env_list);
+	pid = fork();
+	if (pid == 0)
+	{
+		handle_child_process(node, env_list, enva);
+	}
+	else if (pid > 0)
+		waitpid(pid, &status, 0);
+	else
+		perror("fork");
 }
 
-char	*get_cmd_path(t_env_list *env_list, char *cmd)
+static void	handle_child_process(t_cmd_node *node,
+				t_env_list *env_list, char **enva)
+{
+	char	*cmd_path;
+
+	cmd_path = get_cmd_path(env_list, node->cmd[0]);
+	if (!cmd_path)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(node->cmd[0], 2);
+		ft_putendl_fd(": command not found", 2);
+		exit(127);
+	}
+	else
+	{
+		if (execve(cmd_path, node->cmd, enva) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+static char	*get_cmd_path(t_env_list *env_list, char *cmd)
 {
 	char	**path;
 	char	*part_path;
@@ -25,6 +65,7 @@ char	*get_cmd_path(t_env_list *env_list, char *cmd)
 	int		i;
 
 	path = gc_split(get_env_value(env_list, "PATH"), ':');
+	i = 0;
 	while (path[i])
 	{
 		part_path = gc_strjoin(path[i], "/");

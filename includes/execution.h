722a19c433c8fe71c99a3b20d83677f3fbc68444
Miguel Malek <mimalek@3-G-5.42heilbronn.de>
2025-05-08 18:04:24 +0200

    CHORE(execution.h) -> Just added some function prototypes

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 18:50:45 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/07 17:10:37 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/08 17:39:04 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 # define EXECUTION_H
 
 # include "minishell.h"
+# include "env.h"
 # include <stdbool.h>
 
-void	test_execute(t_cmd_node *node, t_env_list *env_list);
+void	test_execute(t_env_list *env_list, t_cmd_node *node);
 void	execute_builtin(t_cmd_node *node, t_env_list *env_list);
-
+void	execute_external(t_cmd_node *node, t_env_list *env_list);
 #endif

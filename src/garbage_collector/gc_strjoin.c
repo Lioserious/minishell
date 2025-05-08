3d5a14fee3b8af00466c63d6431e07adbbbb91bb
Miguel Malek <mimalek@3-G-5.42heilbronn.de>
2025-05-08 15:52:12 +0200

    ADD(gc_strjoin.c) -> Added a strjoin function with gc_malloc

---
+++ src/garbage_collector/gc_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gc_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 15:50:51 by mimalek           #+#    #+#             */
+/*   Updated: 2025/05/08 15:51:21 by mimalek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s;
+	char	*save;
+	size_t	len1;
+	size_t	len2;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (s1)
+		len1 = ft_strlen(s1);
+	else
+		len1 = 0;
+	if (s2)
+		len2 = ft_strlen(s2);
+	else
+		len2 = 0;
+	s = malloc(len1 + len2 + 1);
+	if (!s)
+		return (NULL);
+	save = s;
+	while (s1 && *s1)
+		*s++ = *s1++;
+	while (s2 && *s2)
+		*s++ = *s2++;
+	*s = '\0';
+	return (save);
+}

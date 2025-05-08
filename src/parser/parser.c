5bd0cd77f12c9cb2c291c1977bc306cd738f8bf0
Lioserious <lihrig@student.42heilbronn.de>
2025-05-08 14:19:36 +0200

    FEAT(Parser) -> Parser function takes tokens and distributes them in the command list

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,21 +6,65 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:04:55 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/08 12:42:07 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/08 14:18:06 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+t_token *parse_command(t_token *current, t_cmd_list *cmd_list)
+{
+	t_cmd_node *cmd_node;
+	char **cmd_args;
+	int arg_count;
+	
+	cmd_node = NULL;
+	cmd_args = NULL;
+	arg_count = 0;
+	
+	while(current && current->type != TOKEN_PIPE && current->type != TOKEN_EOF)
+	{
+		if(current->type == TOKEN_WORD || current->type == TOKEN_ENV_VAR)
+			current = parse_command_word(current, &cmd_args, &arg_count);
+		else if(is_redirection_token(current->type))
+			current = parse_redirection(current, &cmd_node, &cmd_args, &arg_count);
+		else
+		{
+			error_handler("SYNTAX ERROR: UNEXPECTED TOKEN", STDOUT_FILENO);
+			
+		}
+	}
+		finalize_command(&cmd_node, &cmd_args, arg_count);
+		add_cmd_to_list(cmd_list, cmd_node);
+		return (current);
+}
+t_token *parse_command_word(t_token *current, char ***cmd_args, int *arg_count)
+{
+    (*arg_count)++;
+    *cmd_args = realloc_cmd_args(*cmd_args, *arg_count);
+    
+    if (current->type == TOKEN_ENV_VAR)
+        (*cmd_args)[*arg_count - 1] = expand_env_var(current->value);
+    else
+        (*cmd_args)[*arg_count - 1] = gc_strdup(current->value);
+    return (current->next);
+}
 t_cmd_list *parser(t_token_list *token_list)
 {
-	t_cmd_list *cmd_list = create_cmd_list();
-	t_token *current;
-	
-	t_token *current = token_list->head;
+    t_cmd_list *cmd_list;
+    t_token *current;
+    
+    if (!token_list || !token_list->head)
+        return (NULL);
+    cmd_list = create_cmd_list();
+    if (!cmd_list)
+        return (NULL);
+    current = token_list->head;
 	while (current && current->type != TOKEN_EOF)
 	{
-		
+		current = parse_command(current, cmd_list);
+		if (current && current->type == TOKEN_PIPE)
+			current = current->next;
 	}
 	return (cmd_list);
 }
--- src/parser/parser_helper.c
+++ src/parser/parser_helper.c
@@ -6,7 +6,14 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 12:58:09 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/08 12:58:09 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/08 13:04:57 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+int is_redirection_token(t_token_type type)
+{
+    return (type == TOKEN_REDIR_OUT || type == TOKEN_REDIR_IN || 
+            type == TOKEN_REDIR_APPEND || type == TOKEN_REDIR_HEREDOC);
+}

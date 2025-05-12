c0106368ec725375fab49a6f644af67f7c903de5
Lioserious <lihrig@student.42heilbronn.de>
2025-05-12 12:32:27 +0200

    BUILD(CMD_TYPES) -> Command types

--- includes/cmd_types.h
+++ includes/cmd_types.h
@@ -6,16 +6,27 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:00:59 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/02 12:02:28 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/12 12:31:47 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CMD_TYPES_H
 # define CMD_TYPES_H
 
-# define CMD_BUILTIN 1  // Eingebaute Befehle
-# define CMD_EXTERNAL 2 // Externe Programme
-# define CMD_PIPE 3     // Pipe-Operator
-# define CMD_REDIR 4    // Umleitungsoperator
+typedef enum e_cmd_type
+{
+    CMD_SIMPLE = 1,     /* Ein einfacher Befehl (ls -la) */
+    CMD_PIPE = 2,       /* Teil einer Pipeline (cmd | cmd) */
+    CMD_BUILTIN = 3,    /* Eingebauter Befehl (cd, echo, etc.) */
+}   t_cmd_type;
+
+/* Redirection Types */
+typedef enum e_redir_type
+{
+    REDIR_IN = 1,       /* Input Redirection (<) */
+    REDIR_OUT = 2,      /* Output Redirection (>) */
+    REDIR_APPEND = 3,   /* Append Output (>>) */
+    REDIR_HEREDOC = 4   /* Here Document (<<) */
+}   t_redir_type;
 
 #endif

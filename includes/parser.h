42adbf90c1e99414ebdeba038921154e4981583b
Lioserious <lihrig@student.42heilbronn.de>
2025-05-12 14:40:23 +0200

    BUILT(HEADER) parser header

--- includes/parser.h
+++ includes/parser.h
@@ -6,17 +6,18 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 12:58:35 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/12 14:19:04 by lihrig           ###   ########.fr       */
+/*   Updated: 2025/05/12 14:26:56 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
+# include "minishell.h"
 // helper
 int		is_redirection_token(t_token_type type);
 char	**realloc_cmd_args(char **args, int new_size);
-void	print_parsed_cmd_list(t_cmd_list *cmd_list)
-t_token *parse_redirections(t_token *token, t_cmd_node *cmd);
+void	print_parsed_cmd_list(t_cmd_list *cmd_list);
+t_token	*parse_redirections(t_token *token, t_cmd_node *cmd);
 
 #endif

afd2932f639f07c2659eb0f3b25f74f3dc1b4024
Miguel Malek <mimalek@2-F-2.42heilbronn.de>
2025-05-13 16:52:37 +0200

    CHORE(main.c) -> Just commented a bit from the previous code

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:31:24 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/13 13:25:56 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/13 14:04:40 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ int	main(void)
 	init_env(env_list);
 	// Begrüßung anzeigen
 	ft_putendl_fd("Welcome to Minishell Test!", STDOUT_FILENO);
-	ft_putendl_fd("First, running automatic test cases...", STDOUT_FILENO);
-	// Automatische Tests für Tokenizer und Parser ausführen
-	run_tokenizer_tests();
-	run_parser_tests();
-	run_env_var_tests();
-	ft_putendl_fd("\nNow entering interactive mode. Type 'exit' to quit.",
-		STDOUT_FILENO);
-	ft_putendl_fd("Enter commands to see tokenization and parsing results.",
-		STDOUT_FILENO);
+	// ft_putendl_fd("First, running automatic test cases...", STDOUT_FILENO);
+	// // Automatische Tests für Tokenizer und Parser ausführen
+	// run_tokenizer_tests();
+	// run_parser_tests();
+	// run_env_var_tests();
+	// ft_putendl_fd("\nNow entering interactive mode. Type 'exit' to quit.",
+	// 	STDOUT_FILENO);
+	// ft_putendl_fd("Enter commands to see tokenization and parsing results.",
+	// 	STDOUT_FILENO);
 	// Interaktiver Modus
 	while (1)
 	{
@@ -44,8 +44,8 @@ int	main(void)
 		if (input[0] == '\0')
 			continue ;
 		add_history(input);
-		if (ft_strncmp(input, "exit", 5) == 0)
-			break ;
+		// if (ft_strncmp(input, "exit", 5) == 0)
+		// 	break ;
 		// Tokenize the input and display results
 		tokens = tokenizer(input);
 		if (!tokens)
@@ -53,8 +53,8 @@ int	main(void)
 			ft_putendl_fd("ERROR: Tokenization failed!", STDOUT_FILENO);
 			continue ;
 		}
-		ft_putendl_fd("\n--- TOKENIZATION RESULTS ---", STDOUT_FILENO);
-		print_tokenizer_results(tokens);
+		// ft_putendl_fd("\n--- TOKENIZATION RESULTS ---", STDOUT_FILENO);
+		// print_tokenizer_results(tokens);
 		// Parse the tokens and display results
 		cmd_list = parser(tokens);
 		if (!cmd_list)
@@ -62,8 +62,8 @@ int	main(void)
 			ft_putendl_fd("ERROR: Parsing failed!", STDOUT_FILENO);
 			continue ;
 		}
-		ft_putendl_fd("\n--- PARSING RESULTS ---", STDOUT_FILENO);
-		print_parsed_cmd_list(cmd_list);
+		// ft_putendl_fd("\n--- PARSING RESULTS ---", STDOUT_FILENO);
+		// print_parsed_cmd_list(cmd_list);
 		execute(env_list, cmd_list->head);
 	}
 	rl_clear_history();

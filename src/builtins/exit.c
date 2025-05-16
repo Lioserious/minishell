e582eb9bdf81aac1619447b40e9cb8c4077925b1
Miguel Malek <mimalek@3-H-2.42heilbronn.de>
2025-05-16 18:33:16 +0200

    CHORE(exit.c) -> Implemented the use of clean_exit function

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,20 +6,18 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:00:17 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/06 13:03:07 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/16 17:24:52 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_exit(t_cmd_node *node)
+void	ft_exit(t_cmd_node *node)
 {
 	int	status;
 
 	status = 0;
 	if (node->cmd[1])
 		status = ft_atoi(node->cmd[1]);
-	garbage_collector_empty();
-	rl_clear_history();
-	exit(status);
+	clean_exit(status);
 }

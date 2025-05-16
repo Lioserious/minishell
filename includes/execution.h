3e15bec22696c8f65f47e163f67c9f73a041fb93
Miguel Malek <mimalek@3-H-2.42heilbronn.de>
2025-05-16 18:35:50 +0200

    CHORE(execution.h) -> Added the prototype of is_builtin

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 18:50:45 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/15 11:44:39 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/05/16 17:38:03 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@ void	execute_builtin(t_cmd_node *node, t_env_list *env_list);
 void	execute_external(t_cmd_node *node, t_env_list *env_list);
 void	execute_redirections(t_file_list *file_list);
 int		count_cmds(t_cmd_node *node);
+int		is_builtin(t_cmd_node *node);
 #endif

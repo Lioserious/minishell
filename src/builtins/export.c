018310737c00aeb509b3a3c1582821999e6c952a
Miguel Malek <mimalek@3-I-3.42heilbronn.de>
2025-06-02 08:42:03 +0200

    FIX(export.c)->No duplicates now

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,11 +6,13 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 11:12:54 by mimalek           #+#    #+#             */
-/*   Updated: 2025/05/06 15:29:36 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/06/02 08:39:19 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	update_env_var(t_env_list *env_list, char *name, char *value, int is_export);
 
 int	print_export_list(t_env_list	*env_list)
 {
@@ -33,6 +35,24 @@ int	print_export_list(t_env_list	*env_list)
 	return (0);
 }
 
+static void	update_env_var(t_env_list *env_list, char *name, char *value, int is_export)
+{
+	t_env_node	*current;
+
+	current = env_list->head;
+	while (current)
+	{
+		if (ft_strcmp(current->name, name) == 0)
+		{
+			current->value = gc_strdup(value);
+			current->is_export = is_export;
+			return ;
+		}
+		current = current->next;
+	}
+	ft_add_env_var(env_list, name, value, is_export);
+}
+
 int	ft_export(t_env_list *env_list, t_cmd_node *node)
 {
 	int		i;
@@ -50,11 +70,11 @@ int	ft_export(t_env_list *env_list, t_cmd_node *node)
 		{
 			name = gc_substr(node->cmd[i], 0, equal - node->cmd[i]);
 			value = gc_strdup(equal + 1);
-			ft_add_env_var(env_list, name, value, 1);
+			update_env_var(env_list, name, value, 1);
 		}
 		else
 		{
-			ft_add_env_var(env_list, node->cmd[i], "", 1);
+			update_env_var(env_list, node->cmd[i], "", 1);
 		}
 		i++;
 	}

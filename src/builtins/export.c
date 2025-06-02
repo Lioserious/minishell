faa4221aaf6c3e2c345eead5e280856799b490d3
Miguel Malek <mimalek@3-I-3.42heilbronn.de>
2025-06-02 09:24:56 +0200

    FEAT(export.c) -> Added '+=' (append) logic

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 11:12:54 by mimalek           #+#    #+#             */
-/*   Updated: 2025/06/02 08:39:19 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/06/02 09:23:55 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,14 @@ int	print_export_list(t_env_list	*env_list)
 		{
 			ft_putstr_fd("declare -x ", 1);
 			ft_putstr_fd(current->name, 1);
-			if (ft_strlen(current->value)
-				&& ft_strncmp(current->value, " ", 2) != 0)
+			if (current->value)
+			{
 				ft_putchar_fd('=', 1);
-			ft_putendl_fd(current->value, 1);
+				ft_putchar_fd('"', 1);
+				ft_putstr_fd(current->value, 1);
+				ft_putchar_fd('"', 1);
+			}
+			ft_putchar_fd('\n', 1);
 		}
 		current = current->next;
 	}
@@ -53,20 +57,52 @@ static void	update_env_var(t_env_list *env_list, char *name, char *value, int is
 	ft_add_env_var(env_list, name, value, is_export);
 }
 
+static void	append_env_var(t_env_list *env_list, char *name, char *value, int is_export)
+{
+	t_env_node	*current;
+	char		*new_value;
+
+	current = env_list->head;
+	while (current)
+	{
+		if (ft_strcmp(current->name, name) == 0)
+		{
+			if (current->value)
+				new_value = gc_strjoin(current->value, value);
+			else
+				new_value = gc_strdup(value);
+			current->value = new_value;
+			current->is_export = is_export;
+			return ;
+		}
+		current = current->next;
+	}
+	new_value = gc_strdup(value);
+	ft_add_env_var(env_list, name, new_value, is_export);
+}
+
 int	ft_export(t_env_list *env_list, t_cmd_node *node)
 {
 	int		i;
 	char	*equal;
 	char	*name;
 	char	*value;
+	char	*append;
 
 	if (!node->cmd[1])
 		return (print_export_list(env_list));
 	i = 1;
 	while (node->cmd[i])
 	{
+		append = ft_strnstr(node->cmd[i], "+=", ft_strlen(node->cmd[i]));
 		equal = ft_strchr(node->cmd[i], '=');
-		if (equal)
+		if (append)
+		{
+			name = gc_substr(node->cmd[i], 0, append - node->cmd[i]);
+			value = gc_strdup(append + 2);
+			append_env_var(env_list, name, value, 1);
+		}
+		else if ((equal = ft_strchr(node->cmd[i], '=')))
 		{
 			name = gc_substr(node->cmd[i], 0, equal - node->cmd[i]);
 			value = gc_strdup(equal + 1);

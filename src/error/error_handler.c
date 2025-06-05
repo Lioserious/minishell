5fa7207b1d8862dfd5ad5533df4981a753745751
Lioserious <lihrig@student.42heilbronn.de>
2025-06-05 15:54:01 +0200

    ADD() -> new function that takes care of the exit code

--- src/error/error_handler.c
+++ src/error/error_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_handler.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:29:40 by lihrig            #+#    #+#             */
-/*   Updated: 2025/05/26 08:43:06 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/06/05 15:52:37 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,16 @@ void	*error_handler(char *message, int status)
 	ft_putstr_fd("\n", STDERR_FILENO);
 	if (status > 0)
 		clean_exit(status);
+	return (NULL);
+}
+
+void	*error_handler_exit_code(char *message, int exit_code,
+		t_env_list *env_list)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(message, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	if (env_list)
+		env_list->last_exitcode = exit_code;
 	return (NULL);
 }

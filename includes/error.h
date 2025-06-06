e04a49b1db90f94d16bf9c590b883137fde44146
Miguel Malek <mimalek@3-I-2.42heilbronn.de>
2025-06-06 12:40:01 +0200

    FEAT(exit) -> Added a better exit logic for the first files in execution

--- includes/error.h
+++ includes/error.h
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:01:26 by lihrig            #+#    #+#             */
-/*   Updated: 2025/06/05 15:08:38 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/06/06 12:14:29 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 
 void	*error_handler(char *message, t_env_list *env_list);
 void	clean_exit(t_env_list *env_list);
+void	update_exit_status(t_env_list *env_list, int status);
 
 #endif

bfce1af1fde6225aded351d0147f211e6a9f8012
Miguel Malek <mimalek@3-I-2.42heilbronn.de>
2025-06-06 13:45:43 +0200

    Refactored error handler function

--- src/error/error_handler.c
+++ src/error/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: mimalek <mimalek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 12:29:40 by lihrig            #+#    #+#             */
-/*   Updated: 2025/06/05 15:24:48 by mimalek          ###   ########.fr       */
+/*   Updated: 2025/06/06 13:42:03 by mimalek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,10 @@ void	*error_handler(char *message, t_env_list *env_list)
 	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	ft_putstr_fd(message, STDERR_FILENO);
 	ft_putstr_fd("\n", STDERR_FILENO);
-	if (env_list->last_exitcode > 0)
-	{
-		restore_std_fds(env_list);
-		garbage_collector_empty();
-		rl_clear_history();
-		exit(1);
-	}
+	restore_std_fds(env_list);
+	garbage_collector_empty();
+	rl_clear_history();
+	env_list->last_exitcode = 1;
+	exit(1);
 	return (NULL);
 }
